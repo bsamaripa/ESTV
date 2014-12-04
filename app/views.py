3a2530955b30521a2d0c3d50c63d5ebef25b4730
Byron Samaripa <Byron.Samaripa@colorado.edu>
2014-12-04 13:02:27 -0700

    Add CI via Travis

--- app/views.py
+++ app/views.py
@@ -3,8 +3,8 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html',
-                           title='ESTV - Index')
+    return 'INDEX!'
+
 @app.route('/event')
 def event():
     return 'ESL ONE!'
