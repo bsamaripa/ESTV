645ed2c155d7e08709b15d08be23f9f421096a87
Byron Samaripa <Byron.Samaripa@gmail.com>
2014-12-10 11:23:18 -0700

    Add URL error handling

--- app/models.py
+++ app/models.py
@@ -7,3 +7,28 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), index=True, unique=True)
+    game = db.relationship('Game', backref ='event', lazy='dynamic')
+    teams = db.Column(db.String(1024), index=True, unique=True)
+    bracket = db.Column(db.String(8000), index=True, unique=True)
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), index=True, unique=True)
+    icon = db.Column(db.LargeBinary)
+
+class Team(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), index=True, unique=True)
+    game = db.relationship('Game', backref='team', lazy='dynamic')
+    players = db.relationship('Player', backref='team', lazy='dynamic')
+
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), index=True, unique=True)
+    team = db.relationship('Team', backref='player', lazy='dynamic')
+
+
--- app/views.py
+++ app/views.py
@@ -46,6 +46,15 @@ def eventInfo():
 def user():
     return 'Users coming soon!'
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
+
 #  RESTful API
 @auth.error_handler
 def unauthorized():
